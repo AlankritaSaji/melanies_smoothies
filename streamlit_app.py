adc189b50b9734cbc2171a8488e82441a4bdd8c4
AlankritaSaji <alan.saji96@gmail.com>
2025-03-15 21:45:28 +0530

    Update streamlit_app.py
    rearrangement 33-34

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,5 @@ if ingredients_list:
 	if time_to_insert:
 		session.sql(my_insert_stmt).collect()
 		st.success('Your Smoothie is ordered!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+		st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
